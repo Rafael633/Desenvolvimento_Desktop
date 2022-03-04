0553615263950de8327eb3893cc373b94854c3e0
Rafael Souza <88410769+Rafael633@users.noreply.github.com>
2022-03-04 14:14:45 -0300

    Atividade com Integração

--- projetoConsultorio/Models/Dentista.cs
+++ projetoConsultorio/Models/Dentista.cs
@@ -1,16 +1,23 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using Repository;
 
 namespace Models
 {
     public class Dentista : Pessoa
     {
-        public static int ID = 0;
-        private static List<Dentista> Dentistas = new List<Dentista>();
+        [Required]
         public string Registro { set; get; }
+        [Required]
         public double Salario { set; get; }
-        public int IdEspecialidade { set; get; }
-        public Especialidade Especialidade { get; }
+        public Especialidade Especialidade { set; get; }
+        [Required]
+        public int EspecialidadeId { set; get; }
 
+        public Dentista() { }
+        
         public Dentista(
             string Nome,
             string Cpf,
@@ -19,49 +26,36 @@ namespace Models
             string Senha,
             string Registro,
             double Salario,
-            int IdEspecialidade
-        ) : this(++ID, Nome, Cpf, Fone, Email, Senha, Registro, Salario, IdEspecialidade)
-        {
-        }
-
-        private Dentista(
-            int Id,
-            string Nome,
-            string Cpf,
-            string Fone,
-            string Email,
-            string Senha,
-            string Registro,
-            double Salario,
-            int IdEspecialidade
-        ) : base(Id, Nome, Cpf, Fone, Email, Senha)
+            int EspecialidadeId
+        ) : base(Nome, Cpf, Fone, Email, Senha)
         {
             this.Registro = Registro;
             this.Salario = Salario;
-            this.IdEspecialidade = IdEspecialidade;
-            this.Especialidade = Especialidade.GetEspecialidades().Find(Especialidade => Especialidade.Id == IdEspecialidade);
+            this.EspecialidadeId = EspecialidadeId;
 
-            Dentistas.Add(this);
+            Context db = new Context();
+            db.Dentistas.Add(this);
+            db.SaveChanges();
         }
-
+        
         public override string ToString()
         {
             return base.ToString()
-                + $"\nRegistro (CRO): {this.Registro}" 
-                + $"\nSalario: R$ {this.Salario}"
-                + $"\nEspecialiade: {this.Especialidade}";
+                + $"\n Registro (CRO): {this.Registro}"
+                + $"\n Salario: R$ {this.Salario}"
+                + $"\n Especialidade: {this.Especialidade}";
         }
-
 
         public static List<Dentista> GetDentistas()
         {
-            return Dentistas;
+            Context db = new Context();
+            return (from Dentista in db.Dentistas select Dentista).ToList();
         }
 
         public static void RemoverDentista(Dentista dentista)
         {
-            Dentistas.Remove(dentista);
+            Context db = new Context();
+            db.Dentistas.Remove(dentista);
         }
-
     }
 }
